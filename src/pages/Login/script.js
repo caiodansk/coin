c3f43a5ea4bfb891d0056e0a8f88de9e34c0566d
samuelamorimm <samuelhenriqueamorim@gmail.com>
2024-08-01 17:54:27 -0300

    feat: update db

--- src/pages/Login/script.js
+++ src/pages/Login/script.js
@@ -16,5 +16,48 @@ document.getElementById("buttonPositionUp").addEventListener("click", function()
 })
 // configurando posição do separator
 
+const emailSignUp = document.querySelector("#emailSignUp")
+const passwordSignUp = document.querySelector("#passwordSignUp")
+const passwordConfirmSignUp = document.querySelector("#confirmPasswordSignUp")
+const buttonSignUp = document.querySelector("#buttonSignUp")
+
+
+
+function cadastrar(emailUp, passwordUp, passwordConfirmUp) {
+    emailUp = emailSignUp.value
+    passwordUp = passwordSignUp.value
+    passwordConfirmUp = passwordConfirmSignUp.value
+
+    if (passwordUp !== passwordConfirmUp) {
+        alert("As senhas inseridas são diferentes")
+        return
+    }
+    if (!emailUp || !passwordUp) {
+        alert("Por favor, preencha todos os campos.");
+        return;
+    }
+
+    var user = {
+        "email": emailSignUp.value,
+        "password": passwordSignUp.value,
+    }
+    salvarDb(user)
+}
+
+function salvarDb(user){
+    let users = []
+
+    if(localStorage.getItem("usuarios"))
+    {
+        users = JSON.parse(localStorage.getItem("usuarios"))  
+    } 
+    users.push(user)
+    localStorage.setItem("usuarios", JSON.stringify(users))
+    
+
+}
+
+
+
 
 
